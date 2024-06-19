4e3e3916ade19f617c078df70d2f0c6872c33146
RadicalGarlic <nobody@nowhere>
2024-06-18 23:01:28 -0700

    Add wiki route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { WikiComponent } from './wiki/wiki.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'wiki',
+    component: WikiComponent
+  }
+];
---
+++ src/app/wiki/wiki.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-wiki',
+  standalone: true,
+  templateUrl: './wiki.component.html',
+  styleUrl: './wiki.component.sass'
+})
+export class WikiComponent {
+
+}
