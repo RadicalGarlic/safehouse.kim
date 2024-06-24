0b7941d5a9acaf692ac26d45975f301c107355e6
RadicalGarlic <nobody@nowhere>
2024-06-24 00:37:43 -0700

    Added anchor links

--- src/app/cy-borg/cy-borg-lore/cy-borg-lore.component.ts
+++ src/app/cy-borg/cy-borg-lore/cy-borg-lore.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cy-borg-lore',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './cy-borg-lore.component.html',
   styleUrl: './cy-borg-lore.component.scss'
 })
 export class CyBorgLoreComponent {
+  constructor(private viewportScroller: ViewportScroller) {}
 
+  public scrollToId(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
 }
