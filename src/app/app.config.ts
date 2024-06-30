713eeca398e44b986e50a47b2cf4b24b52d51ccb
RadicalGarlic <nobody@nowhere>
2024-06-30 12:49:35 -0700

    Enable links to anchors and back nav scroll mem

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  InMemoryScrollingFeature,
+  InMemoryScrollingOptions,
+  withInMemoryScrolling
+} from '@angular/router';
 
 import { routes } from './app.routes';
 
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature = withInMemoryScrolling(scrollConfig);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, inMemoryScrollingFeature)
+  ]
 };
