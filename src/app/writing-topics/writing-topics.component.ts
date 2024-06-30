5b648b8e35b5f9d2b312b5d18bfae1f457de1b7a
RadicalGarlic <nobody@nowhere>
2024-06-30 12:38:27 -0700

    Link Tekken 7 as writing topic

--- src/app/writing-topics/writing-topics.component.ts
+++ src/app/writing-topics/writing-topics.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-writing-topics',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './writing-topics.component.html',
   styleUrl: './writing-topics.component.scss'
 })
