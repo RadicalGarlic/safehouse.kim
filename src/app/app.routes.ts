a8352d106bafd21691550d7307303c90a50fa41a
RadicalGarlic <nobody@nowhere>
2024-07-02 21:21:26 -0700

    Add shopping-list and route path strings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,29 +7,38 @@ import { CyBorgComponent } from './cy-borg/cy-borg.component';
 import { CyBorgLoreComponent } from './cy-borg/cy-borg-lore/cy-borg-lore.component';
 import { Tekken7Component } from './tekken-7/tekken-7.component';
 
+export const RoutePaths = {
+  wiki: 'wiki',
+  writingTopics: 'writing-topics',
+  cyBorg: 'cy_borg',
+  cyBorgLore: 'cy_borg/lore',
+  tekken7: 'tekken-7',
+  shoppingList: 'shopping-list',
+}
+
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
   {
-    path: 'wiki',
+    path: RoutePaths.wiki,
     component: WikiComponent,
   },
   {
-    path: 'writing-topics',
+    path: RoutePaths.writingTopics,
     component: WritingTopicsComponent,
   },
   {
-    path: 'cy_borg',
+    path: RoutePaths.cyBorg,
     component: CyBorgComponent,
   },
   {
-    path: 'cy_borg/lore',
+    path: RoutePaths.cyBorgLore,
     component: CyBorgLoreComponent,
   },
   {
-    path: 'tekken-7',
+    path: RoutePaths.tekken7,
     component: Tekken7Component
   }
 ];
